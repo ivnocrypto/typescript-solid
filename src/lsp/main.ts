f8e2f8da2400c64a70fdebe4157174ddae9ad4f0
ivnogg <ivnocrypto@gmail.com>
2023-01-13 01:46:00 -0300

    add principio substituicao de Liskov

---
+++ src/lsp/main.ts
@@ -0,0 +1,29 @@
+import { Messaging } from './services/messaging';
+import { Order } from './classes/order';
+import { Persistency } from './persistency';
+import { ShoppingCart } from './classes/shopping-cart';
+import { Product } from './classes/product';
+import { NoDiscount } from './classes/discount';
+
+// = == =========INJEÇÃO DEPENDÊNCIA===================== == =
+
+// const fiftyPercentDiscount = new FiftyPercentDiscount();
+// const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+// = == =================================================== == =
+
+shoppingCart.addItem(new Product('Camiseta', 49.9));
+shoppingCart.addItem(new Product('Caderno', 9.9));
+shoppingCart.addItem(new Product('Lápis', 1.59));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+console.log(order.orderStatus);
+order.checkOut();
+console.log(order.orderStatus);
